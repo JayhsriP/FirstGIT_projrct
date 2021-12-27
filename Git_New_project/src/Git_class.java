9c0b8c8699e9967d5c51f10376c57dd74bd0e85c
JayhsriP <96289061+JayhsriP@users.noreply.github.com>
2021-12-27 10:01:08 +0530

    Update Git_class.java

--- Git_New_project/src/Git_class.java
+++ Git_New_project/src/Git_class.java
@@ -3,10 +3,15 @@
 
 public class Git_class {
 
+	public void add()
+	{
+		System.out.println("Addition mthd");	
+	}
 	public static void main(String[] args) {
 		
 		
 		System.out.println("hi this is git program");
 		System.out.println("done some changes");
+		
 	}
 }
